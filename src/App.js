f5bc85a58fc2aec25efe73feb11dbe7a43db1653
SalazarSDF <q37076111@gmail.com>
2022-07-12 15:43:46 +0300

    component didmount still learning react class components

--- src/App.js
+++ src/App.js
@@ -1,54 +1,39 @@
 import { Component } from "react";
-
 import logo from "./logo.svg";
 import "./App.css";
 
+//https://jsonplaceholder.typicode.com/users
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: { firstName: "Jopa", lastName: "OSLINAYA" },
-      company: "Hoody",
+      monsters: [],
     };
-    this.index = 0;
-    this.arr = ["jopa", "vasya", "misha"];
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{" "}
-            {this.state.company}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  return {
-                    name: {
-                      firstName: this.arr[this.index],
-                      lastName: "Depard'e",
-                    },
-                  };
-                },
-                () => {
-                  if (this.index >= 2) {
-                    this.index = 0;
-                  } else {
-                    this.index++;
-                  }
-                  console.log(this.state);
-                  console.log(this.index);
-                }
-              );
-              //console.log(this.state);
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+        ))}
       </div>
     );
   }
