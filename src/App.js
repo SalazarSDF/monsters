593eea4b2be89e150768dff1b3d1e67937825212
SalazarSDF <q37076111@gmail.com>
2022-07-12 14:38:28 +0300

    done: with setState theory

--- src/App.js
+++ src/App.js
@@ -4,22 +4,50 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: { firstName: "Jopa", lastName: "OSLINAYA" },
+      company: "Hoody",
+    };
+    this.index = 0;
+    this.arr = ["jopa", "vasya", "misha"];
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload. JOPA
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{" "}
+            {this.state.company}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <button
+            onClick={() => {
+              this.setState(
+                () => {
+                  return {
+                    name: {
+                      firstName: this.arr[this.index],
+                      lastName: "Depard'e",
+                    },
+                  };
+                },
+                () => {
+                  if (this.index >= 2) {
+                    this.index = 0;
+                  } else {
+                    this.index++;
+                  }
+                  console.log(this.state);
+                  console.log(this.index);
+                }
+              );
+              //console.log(this.state);
+            }}
           >
-            Learn React
-          </a>
+            Change Name
+          </button>
         </header>
       </div>
     );
